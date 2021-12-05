118c0e171cee1a8aac9ad3ce38b12d812f4374f0
rovshanguli <rovshanshg@code.edu.az>
2021-12-05 18:44:53 +0400

    Calculator

---
+++ CalculateProject/CalculateProject/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using ClassLibrary1;
+
+
+namespace CalculateProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator hesabla = new Calculator();
+            hesabla.Calcutlate();
+
+
+
+
+
+            //Console.WriteLine("Toplama ucun + secin");
+            //Console.WriteLine("Cixma ucun - secin");
+            //Console.WriteLine("Vurma ucun * secin");
+            //Console.WriteLine("Bolme ucun / secin");
+            //Console.WriteLine("1-ci ededi daxil edin");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Edeceyiniz emeliyyati secin");
+            //string emel = Console.ReadLine();
+            //Console.WriteLine("2-ci ededi daxil edin");
+            //int m = Convert.ToInt32(Console.ReadLine());
+            //int netice = 0;
+            //switch (emel)
+            //{
+            //    case "+":
+            //        {
+            //            netice = n + m;
+            //            Console.WriteLine($"Netice = {netice}");
+            //            return;
+            //        }
+            //    case "-":
+            //        {
+            //            netice = n - m;
+            //            Console.WriteLine($"Netice = {netice}");
+            //            return;
+            //        }
+            //    case "*":
+            //        {
+            //            netice = n * m;
+            //            Console.WriteLine($"Netice = {netice}");
+            //            return;
+            //        }
+            //    case "/":
+            //        {
+            //            netice = n / m;
+            //            Console.WriteLine($"Netice = {netice}");
+            //            return;
+            //        }
+
+
+            //}
+
+
+            
+
+           
+           
+            
+            
+
+
+        }
+    }
+}
---
+++ CalculateProject/ClassLibrary1/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Calculator
+    {
+        public void Calcutlate()
+        {
+            Console.WriteLine("Toplama ucun + secin");
+            Console.WriteLine("Cixma ucun - secin");
+            Console.WriteLine("Vurma ucun * secin");
+            Console.WriteLine("Bolme ucun / secin");
+            Console.WriteLine("1-ci ededi daxil edin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Edeceyiniz emeliyyati secin");
+            string emel = Console.ReadLine();
+            Console.WriteLine("2-ci ededi daxil edin");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int netice = 0;
+            switch (emel)
+            {
+                case "+":
+                    {
+                        netice = n + m;
+                        Console.WriteLine($"Netice = {netice}");
+                        return;
+                    }
+                case "-":
+                    {
+                        netice = n - m;
+                        Console.WriteLine($"Netice = {netice}");
+                        return;
+                    }
+                case "*":
+                    {
+                        netice = n * m;
+                        Console.WriteLine($"Netice = {netice}");
+                        return;
+                    }
+                case "/":
+                    {
+                        netice = n / m;
+                        Console.WriteLine($"Netice = {netice}");
+                        return;
+                    }
+
+
+            }
+            
+
+        }
+    }
+}
